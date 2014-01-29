4492edf10faed5ad55be1ef1de0d9f721178a65c
Jeff McCoy <code@jeffandshauna.net>
2014-01-29 05:11:27 -0500

    Added Icon & Sorter directives

--- app/js/directives.js
+++ app/js/directives.js
@@ -46,4 +46,77 @@
 		};
 	});
 
+	app.directive('icon', function () {
+
+		return {
+			'restrict': 'E',
+			'replace' : true,
+			'template': '<img ng-src="{{icon}}" class="icon" title="{{hover}}"/>',
+			'scope'   : {
+				'path' : '@',
+				'hover': '@'
+			},
+			'link'    : function ($scope) {
+
+				$scope.icon = FTSS.icons[$scope.path];
+
+			}
+		};
+
+	});
+
+	app.directive('sorter', function () {
+
+		return {
+			'restrict': 'A',
+			'template': '<a href="" ng-click="sort()">{{name}}</a>',
+			'scope'   : true,
+			'link'    : function ($scope, $el, $attrs) {
+
+				var last, field, $parent;
+
+				field = $attrs.sorter;
+				$parent = $scope.$parent;
+				$scope.name = $attrs.text || $attrs.sorter;
+
+				$scope.sort = function () {
+
+					var sorter = function (data) {
+
+						return last ? _.sortBy(data, field).reverse() : _.sortBy(data, field);
+
+					};
+
+					last = (last === field) ? false : field;
+
+					if ($parent.groups) {
+
+						_.each($parent.groups, function (data, group) {
+
+							$parent.groups[group] = sorter(data);
+
+						});
+
+					} else {
+
+						$parent.data = sorter($parent.data);
+
+					}
+
+					$el.addClass('active').siblings().removeClass('active');
+					$('.sorter-'+field).addClass('active').siblings().removeClass('active');
+
+				};
+
+				if ($attrs.hasOwnProperty('default')) {
+
+					$parent.sort = $scope.sort;
+
+				}
+
+			}
+		}
+
+	});
+
 }());
