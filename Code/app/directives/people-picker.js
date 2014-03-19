6133e8fc08141b2a627c5c9b8744a0e6576b547f
Jeff McCoy <code@jeffandshauna.net>
2014-03-19 02:59:42 -0400

    Add ngIf bug workaround for peoplePicker directive
    See https://github.com/angular/angular.js/issues/6571 for details

--- Code/app/directives/people-picker.js
+++ Code/app/directives/people-picker.js
@@ -14,19 +14,27 @@
 		return {
 			'restrict'   : 'E',
 			'templateUrl': '/partials/people-picker.html',
-			'replace'    : true,
+			'priority'   : 700,
 			'link'       : function ($scope, $el, $attrs) {
 
-				var list = $attrs.filter ? _(caches[$attrs.filter]).pluck($attrs.field) : false;
+				if ($scope.createData) {
 
-				$scope.picker = {
-					'field' : $attrs.field,
-					'filter': list ? function (data) {
+					var list = $attrs.filter ? _(caches[$attrs.filter]).pluck($attrs.field) : false;
 
-						return !_(list).contains(data.Id);
+					$scope.picker = {
+						'field' : $attrs.field,
+						'filter': list ? function (data) {
 
-					} : false
-				};
+							return !_(list).contains(data.Id);
+
+						} : false
+					};
+
+				} else {
+
+					$el.remove();
+
+				}
 
 			}
 		};
