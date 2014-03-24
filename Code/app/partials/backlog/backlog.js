8ab7121d029a8957aa54057fb1a753d93d19c4ef
Jeff McCoy <code@jeffandshauna.net>
2014-03-23 23:55:12 -0400

    Significant work on backlog page
    - Added more sort/grouping modes
    - Added timeAvg & maxTime data for all courses and individual courses
    - Filled out wells with real data including time data & unit course
    availability
    - Added new #main-stats styled stat boxes at the top with aggregate
    backlog info

--- Code/app/partials/backlog/backlog.js
+++ Code/app/partials/backlog/backlog.js
@@ -9,23 +9,37 @@ FTSS.ng.controller(
 		function ($scope) {
 
 			var self = FTSS.controller($scope, {
-				'sort' : 'DateNeeded',
-				'group': 'Date',
+					'sort' : 'PDS',
+					'group': 'MDS',
 
-				'grouping': {
-					'Date'         : 'Month',
-					'Unit.LongName': 'FTD',
-					'Course.PDS'   : 'PDS',
-					'Course.MDS'   : 'MDS',
-					'Course.AFSC'  : 'AFSC'
+					'grouping': {
+						'MDS' : 'MDS',
+						'PDS' : 'PDS',
+						'AFSC': 'AFSC'
+					},
+
+					'sorting': {
+						'PDS'         : 'PDS',
+						'days'        : 'Wait Time',
+						'requirements': '# Requirements'
+					},
+
+					'model': 'students'
+
+				}),
+
+				timeAvg = function (time, length) {
+
+					return moment.duration(Math.ceil(time.days / length), 'days').humanize();
+
+
 				},
 
-				'sorting': {
-					'DateNeeded': 'Date'
-				},
-				'model'  : 'requirements'
+				timeMax = function (reqs) {
 
-			});
+					return moment.duration(_.chain(reqs).pluck('days').max().value(), 'days').humanize();
+
+				};
 
 			self
 
@@ -33,26 +47,86 @@ FTSS.ng.controller(
 
 				.then(function (data) {
 
+					      var reqs = {};
+
+					      $scope.totals = {
+						      'max'     :
+							      [
+							      ],
+						      'days'    : 0,
+						      'students': 0,
+						      'reqs'    : 0
+					      };
+
+					      _(data).each(function (s) {
+
+						      if (!s.Archived && !_(s.Requirements_JSON).isEmpty()) {
+
+							      var momentObj = moment(s.ProcessDate);
+
+							      s.days = moment().diff(momentObj, 'days');
+							      s.waited = momentObj.fromNow(true);
+
+							      s.HostUnit = caches.Hosts[s.HostUnitId];
+							      s.FTD = caches.Units[s.HostUnit.FTD];
+
+							      $scope.totals.days += s.days;
+							      $scope.totals.max.push(s.days);
+							      $scope.totals.students++;
+
+							      _(s.Requirements_JSON).each(function (r) {
+
+								      $scope.totals.reqs++;
+
+								      var req = reqs[r];
+
+								      if (!req) {
+									      req = reqs[r] = _(caches.MasterCourseList[r]).clone();
+									      req.days = 0;
+									      req.requirements =
+									      [
+									      ];
+								      }
+
+								      req.requirements.push(s);
+
+								      req.days += s.days;
+
+							      });
+
+						      }
+					      });
+
+					      _(caches.Units).each(function (u) {
+
+						      _(u.Courses_JSON).each(function (c) {
+
+							      reqs[c] && (reqs[c].Units[u.Id] = u);
+
+						      });
+
+					      });
+
+					      $scope.totals.avg = timeAvg($scope.totals, $scope.totals.students).match(/^(\S+)\s(.*)/);
+					      $scope.totals.max = moment.duration(_($scope.totals.max).max(), 'days').humanize().match(/^(\S+)\s(.*)/);
+
+					      data = reqs;
+
 					      self
 
 						      .initialize(data)
 
-						      .then(function (d) {
+						      .then(function (d, k) {
 
-							            d.Course = caches.MasterCourseList[d.CourseId];
-							            d.Unit = caches.Units[d.UnitId];
-							            d.Date = moment(d.DateNeeded).format('MMM YYYY');
+							            // Generate our human-friendly avg wait time
+							            d.avgWait = timeAvg(d, d.requirements.length);
 
-							            d.Students = _.chain(d.Students_JSON)
 
-								            .map(function (c) {
+							            // Generate a human-friendly max wait time
+							            d.maxWait = timeMax(d.requirements);
 
-									                 return caches.Students[c] || false;
 
-								                 })
-
-								            .compact().sort().value();
-							            console.info(d);
+							            console.log(d, k);
 						            });
 
 
