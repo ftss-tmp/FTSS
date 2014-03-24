2af64736a4d35a813ad3f524faf0bfbb6dee03c9
Jeff McCoy <code@jeffandshauna.net>
2014-03-23 23:57:00 -0400

    added utils.distanceCalc for future javascript-only distance calculations

--- Code/app/scripts/ftss-utils.js
+++ Code/app/scripts/ftss-utils.js
@@ -173,6 +173,20 @@ utils.loading = (function () {
 
 }());
 
+utils.distanceCalc = function (lat1, lon1, lat2, lon2) {
+
+	var deg2rad = function (deg) {
+		return deg * (Math.PI / 180);
+	};
+
+	var R = 3963.1676; // Radius of the earth in miles
+	var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+	var dLon = deg2rad(lon2 - lon1);
+	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c; // Distance in miles
+	return d;
+};
 
 /**
  *
