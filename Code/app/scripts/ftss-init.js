e166d4554ca05cf6c7ef076e05440d92fb2c1a19
Jeff McCoy <code@jeffandshauna.net>
2014-03-25 07:20:36 -0400

    Added no space filter from https://gist.github.com/builtbylane/7237798

--- Code/app/scripts/ftss-init.js
+++ Code/app/scripts/ftss-init.js
@@ -84,6 +84,18 @@ var FTSS = {}, utils = {}, caches = {};
 			}
 		]);
 
+	/**
+	 * Description:
+	 *     removes white space from text. useful for html values that cannot have spaces
+	 * Usage:
+	 *   {{some_text | nospace}}
+	 */
+	FTSS.ng.filter('nospace', function () {
+		return function (value) {
+			return (!value) ? '' : value.replace(/ /g, '');
+		};
+	});
+
 	FTSS.people = {};
 	FTSS.utils = {};
 
