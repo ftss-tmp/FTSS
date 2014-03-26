9b36fbd72ce2dc51775bdc450ea551addcaba194
Jeff McCoy <code@jeffandshauna.net>
2014-03-26 19:45:06 -0400

    Milestone: finally implemented complete drag & drop/ auto-upload solution for instructor bio photos
    Bio photos are now seamlessly updated by dragging & dropping the file
    onto the view.  The content is sent SP and the new image loaded using
    the _w pre-generated image from SP.  Caching is not as good as a
    long-expiry (still uses etag) but at least it has that.
    
    jQuery $.ajax function/ $timeout was used instead of $http due to
    issues with SP accepting the ByteArray from it.

--- Code/app/partials/instructors/instructors.js
+++ Code/app/partials/instructors/instructors.js
@@ -1,52 +1,93 @@
-/*global caches, FTSS */
+/*global caches, FTSS, utils, PRODUCTION */
 
-FTSS.ng.controller(
+FTSS.ng.controller('instructorsController',
 
-	'instructorsController',
+                   [
+	                   '$scope',
+	                   '$timeout',
+	                   function ($scope, $timeout) {
 
-	[
-		'$scope',
-		function ($scope) {
+		                   var self = FTSS.controller($scope, {
 
-			var self = FTSS.controller($scope, {
+			                   'sort' : 'Name',
+			                   'group': 'Unit.LongName',
 
-				'sort' : 'Name',
-				'group': 'Unit.LongName',
+			                   'grouping': {
+				                   'Squadron'     : 'Squadron',
+				                   'Unit.LongName': 'Detachment',
+				                   'AFSC'         : 'AFSC'
+			                   },
 
-				'grouping': {
-					'Squadron'     : 'Squadron',
-					'Unit.LongName': 'Detachment',
-					'AFSC'         : 'AFSC'
-				},
+			                   'sorting': {
+				                   'Instructor.Name': 'Name',
+				                   'AFSC'           : 'AFSC'
+			                   },
+			                   'model'  : 'instructors',
 
-				'sorting': {
-					'Instructor.Name': 'Name',
-					'AFSC'           : 'AFSC'
-				},
-				'model'  : 'instructors'
+			                   'edit': function (scope) {
 
-			});
+				                   scope.onFileSelect = function ($files) {
+
+					                   var reader = new FileReader();
+
+					                   reader.onload = function (result) {
+
+						                   var rawBuffer = result.target.result,
+
+							                   rand = utils.generateUUID();
+
+						                   $.ajax({
+							                          'url'        : (PRODUCTION ? 'https://cs3.eis.af.mil/sites/OO-ED-AM-11/FTSS/Prototype' : 'http://dev') + '/_vti_bin/ListData.svc/Bios',
+							                          'type'       : 'POST',
+							                          'data'       : rawBuffer,
+							                          'processData': false,
+							                          'contentType': 'multipart/form-data',
+							                          'headers'    : {
+								                          'accept': "application/json;odata=verbose",
+								                          'slug'  : '/sites/OO-ED-AM-11/FTSS/Prototype/Bios/' + rand + '.jpg'
+							                          },
+							                          'success'    : function () {
+								                          $timeout(function () {
+
+									                          scope.data.Photo = rand;
+									                          scope.modal.$setDirty();
+
+								                          });
+							                          },
+							                          error        : function (err) {
+								                          FTSS.utils.log(err);
+							                          }
+						                          });
+					                   };
+
+					                   reader.readAsArrayBuffer($files[0]);
+
+				                   };
+
+			                   }
+
+		                   });
 
 
-			self
+		                   self
 
-				.bind('loaded')
+			                   .bind('loaded')
 
-				.then(function (data) {
+			                   .then(function (data) {
 
-					      self
+				                         self
 
-						      .initialize(data)
+					                         .initialize(data)
 
-						      .then(function (d) {
+					                         .then(function (d) {
 
-							            d.Unit = caches.Units[d.UnitId];
-							            d.firstName = d.InstructorName.match(/[a-z]+,\s([a-z]+)/i)[1];
+						                               d.Unit = caches.Units[d.UnitId];
+						                               d.firstName = d.InstructorName.match(/[a-z]+,\s([a-z]+)/i)[1];
 
-						            });
+					                               });
 
 
-				      });
+			                         });
 
-		}
-	]);
+	                   }
+                   ]);
