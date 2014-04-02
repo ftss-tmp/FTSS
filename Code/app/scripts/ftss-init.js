e6c2cbfae5dedea6dc6cb03647c3b3aa05f8806e
Jeff McCoy <code@jeffandshauna.net>
2014-04-02 07:07:25 -0400

    Added firstName() & courseNumber() angular filters

--- Code/app/scripts/ftss-init.js
+++ Code/app/scripts/ftss-init.js
@@ -25,7 +25,7 @@ var LOG;
 		                         'monospaced.elastic',
 		                         'partials',
 		                         'angularFileUpload',
-	                             'ngAnimate'
+		                         'ngAnimate'
 	                         ]);
 
 	/*
@@ -40,18 +40,18 @@ var LOG;
 			function ($routeProvider, $modalProvider, $datepickerProvider, $locationProvider) {
 
 				var routes =
-					[
-						'home',
-						'requirements',
-						'scheduled',
-						'requests',
-						'instructors',
-						'students',
-						'catalog',
-						'units',
-						'backlog',
-						'hosts'
-					];
+					    [
+						    'home',
+						    'requirements',
+						    'scheduled',
+						    'requests',
+						    'instructors',
+						    'students',
+						    'catalog',
+						    'units',
+						    'backlog',
+						    'hosts'
+					    ];
 
 				_.each(routes, function (route) {
 
@@ -83,6 +83,7 @@ var LOG;
 			}
 		]);
 
+
 	/**
 	 * Description:
 	 *     removes white space from text. useful for html values that cannot have spaces
@@ -92,6 +93,22 @@ var LOG;
 	FTSS.ng.filter('nospace', function () {
 		return function (value) {
 			return (!value) ? '' : value.replace(/ /g, '');
+		};
+	})
+
+	/**
+	 * Quick filter to return the first name of a person (AF GAL format)
+	 */
+	FTSS.ng.filter('firstName', function () {
+		return function (input) {
+			var match = input && input.match(/[a-z]+,\s([a-z]+)/i);
+			return match && match[1] || input || 'John or Jane Doe';
+		};
+	});
+
+	FTSS.ng.filter('courseNumber', function () {
+		return function (input) {
+			return input || 'Some Random Course?';
 		};
 	});
 
