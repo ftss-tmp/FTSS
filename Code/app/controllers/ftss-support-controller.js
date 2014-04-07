80cdeb7bf823f12d5b49563583ff47b6d9871f74
Jeff McCoy <code@jeffandshauna.net>
2014-04-07 00:57:53 -0400

    Worked support controller code

--- Code/app/controllers/ftss-support-controller.js
+++ Code/app/controllers/ftss-support-controller.js
@@ -1,4 +1,4 @@
-/*global FTSS, _, moment */
+/*global FTSS, _, moment, utils */
 
 /**
  *
@@ -8,26 +8,58 @@
 	"use strict";
 
 	FTSS.ng.controller(
-
 		'supportController',
 		[
 			'$scope',
 			'SharePoint',
-			'$location',
 			'$timeout',
-			function ($scope, SharePoint, $location, $timeout) {
+			function ($scope, SharePoint, $timeout) {
+debugger;
+
+				$scope.$parent.$hide = function () {
+					debugger;
+					$scope.$destroy;
+				};
+
+				$scope.$on('$destroy', function () {
+
+					debugger;
+				});
 
 				$timeout(function () {
 
-					var model = FTSS.models.support, update;
+					var model = FTSS.models.support,
 
-					model.params.$filter = "Page eq '" + $location.path().split('/')[1] + "'";
+					    page = FTSS._fn.getPage(),
+
+					    update;
+
+					model.params.$filter = "Page eq '" + page + "'";
 
 					$scope.addReply = function () {
+
+						var send = {
+							'__metadata': 'Support',
+							'Page'      : page,
+							'Staff'     : false,
+							'Comment'   : 'Test'
+						};
+
+						SharePoint.create(send).then(function (resp) {
+
+							if (resp.status === 201) {
+
+								$scope.comments.push(send);
+
+							}
+
+						}), utils.alert.error;
+
 
 					};
 
 					update = function () {
+						LOG($scope);
 						SharePoint.read(model).then(function (data) {
 
 							_(data).each(function (d) {
