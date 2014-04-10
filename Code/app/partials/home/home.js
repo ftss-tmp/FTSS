84a57e57afe397ad5955126442e51023f5cfbe10
Jeff McCoy <code@jeffandshauna.net>
2014-04-10 14:03:01 -0400

    Added Home to navigation & flickr background

--- Code/app/partials/home/home.js
+++ Code/app/partials/home/home.js
@@ -4,4 +4,16 @@ FTSS.ng.controller('homeController', function () {
 
 	utils.loading(false);
 
+
+	$.getJSON('https://api.flickr.com/services/feeds/photos_public.gne?id=39513508@N06&format=json&jsoncallback=?',
+	          function (resp) {
+
+		          var img = resp.items[
+			          Math.floor(Math.random() * resp.items.length)
+			          ].media.m.replace('_m.', '_c_d.');
+
+		          $('#bg').attr('src', img);
+	          });
+
+
 });
