3e1a37cfb051582e4413fd503dce00f50ce10761
Jeff McCoy <code@jeffandshauna.net>
2014-04-14 15:02:08 -0400

    Home page work
    - Added preferences box & code
    - Added defaults box (needs code)
    - Added catalog updates box
    - Added application updates box

--- Code/app/partials/home/home.js
+++ Code/app/partials/home/home.js
@@ -1,19 +1,66 @@
-/*global app, utils, caches, FTSS, _ */
+/*global app, utils, caches, FTSS, _, moment */
 
-FTSS.ng.controller('homeController', function () {
+(function () {
 
-	utils.loading(false);
+	"use strict";
+
+	FTSS.ng.controller(
+		'homeController',
+
+		['$scope',
+
+		 'SharePoint',
+
+		 function ($scope, SharePoint) {
+
+			 var html = $('html');
+
+			 $scope.pref = FTSS.prefs;
+
+			 SharePoint.read(FTSS.models.updates).then(function (data) {
+
+				 $scope.updates = _(data)
+
+					 .sortBy('Created')
+
+					 .reverse()
+
+					 .map(function (d) {
+						      d.date = moment(d.Created).format('ll');
+						      return d;
+					      })
+
+					 .value();
+
+			 });
+
+			 $scope.$watch(
+				 'cleanSlate', function (res) {
+
+					 if (res) {
+
+						 $scope.courseUpdates = _.filter(caches.MasterCourseList, 'updated');
+
+						 utils.loading(false);
+					 }
+
+				 });
+
+			 $scope.$watch(
+				 'pref', function (val, old) {
+
+					 if (val && val !== old) {
+
+						 localStorage.FTSS_prefs = JSON.stringify(val);
+
+						 html.attr('id', FTSS.prefs.animate ? '' : 'noAnimate');
+
+					 }
+
+				 }, true);
+
+		 }
+		]);
 
 
-	$.getJSON('https://api.flickr.com/services/feeds/photos_public.gne?id=39513508@N06&format=json&jsoncallback=?',
-	          function (resp) {
-
-		          var img = resp.items[
-			          Math.floor(Math.random() * resp.items.length)
-			          ].media.m.replace('_m.', '_c_d.');
-
-		          $('#bg').attr('src', img);
-	          });
-
-
-});
+}());
